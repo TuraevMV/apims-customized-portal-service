4d4bba9472257112241e85f19d1bae60f446b7e9
maximus <turaev.mv@gmail.com>
2020-10-28 13:09:19 +0500

    init

---
+++ src/main/java/apims/cps/ApimsCustomizedPortalServiceApplication.java
@@ -0,0 +1,13 @@
+package apims.cps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimsCustomizedPortalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApimsCustomizedPortalServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apims/cps/ApimsCustomizedPortalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package apims.cps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimsCustomizedPortalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
