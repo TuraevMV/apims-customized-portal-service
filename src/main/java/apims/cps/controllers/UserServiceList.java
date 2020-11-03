da6e48039c696e9c55964e5fb55688e4a27935cb
maximus <turaev.mv@gmail.com>
2020-11-03 23:25:22 +0500

    cps ver 1.0.0

--- src/main/java/apims/cps/controllers/UserServiceList.java
+++ src/main/java/apims/cps/controllers/UserServiceList.java
@@ -2,6 +2,7 @@ package apims.cps.controllers;
 
 import apims.cps.components.DatabaseTools;
 import apims.cps.types.UDBQMParameters;
+import io.jsonwebtoken.*;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -40,17 +41,50 @@ public class UserServiceList {
         HttpHeaders responseHeaders     = new HttpHeaders();
         responseHeaders.setContentType(MediaType.APPLICATION_JSON);
         HttpStatus responseStatus       = HttpStatus.OK;
-        String resultValue;
+        String resultValue = "[]";
 
-        //Формируем параметры для UDBQM
-        BigDecimal userID = BigDecimal.valueOf(37538377);
-        List<UDBQMParameters> listParameters = new ArrayList();
-        UDBQMParameters pParamItem = new UDBQMParameters("BIGDECIMAL",null,null,userID);
-        listParameters.add(pParamItem);
+        log.debug("=========== RequestHeader check ===========");
+            headers.forEach((key, value) -> { log.debug(String.format("Header '%s' = %s", key, value));});
+        log.debug("============================================");
 
-        resultValue = databaseTools.UDBQM(serverType, userServiceListQuery, listParameters );
+        //Откусим подпись. нам не интересно
+        String jwtToken = headers.get("authorization").replaceAll("Bearer ","");
+        int i = jwtToken.lastIndexOf('.');
+        String withoutSignature = jwtToken.substring(0, i+1);
 
+        //Отсечем всякие ошибки просрочки и т.д.
+        if (validateToken(withoutSignature)) {
+            Jwt<Header,Claims> untrusted = Jwts.parser().parseClaimsJwt(withoutSignature);
+            String userUUID = untrusted.getBody().get("userUUID").toString().replace("employee$","");
+            log.debug("userUUID =>" + userUUID);
+            BigDecimal userID = BigDecimal.valueOf(Long.parseLong(userUUID));
+            List<UDBQMParameters> listParameters = new ArrayList();
+            UDBQMParameters pParamItem = new UDBQMParameters("BIGDECIMAL",null,null,userID);
+            listParameters.add(pParamItem);
+            resultValue = databaseTools.UDBQM(serverType, userServiceListQuery, listParameters );
+        }
 
         return new ResponseEntity<>(resultValue, responseHeaders, responseStatus);
     }
+
+
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().parseClaimsJwt(token);
+            return true;
+        } catch (ExpiredJwtException expEx) {
+            log.debug("Token expired");
+        } catch (UnsupportedJwtException unsEx) {
+            log.debug("Unsupported jwt");
+        } catch (MalformedJwtException mjEx) {
+            log.debug("Malformed jwt");
+        } catch (SignatureException sEx) {
+            log.debug("Invalid signature");
+        } catch (Exception e) {
+            log.debug("invalid token");
+        }
+        return false;
+    }
+
 }
